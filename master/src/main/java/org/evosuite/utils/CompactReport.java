b7e465d992aeda0e46e7a4a00855dca9113998c6
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2022-05-06 14:34:19 +0200

    Update CompactReport to provide more statistics output from EvoSuite

--- master/src/main/java/org/evosuite/utils/CompactReport.java
+++ master/src/main/java/org/evosuite/utils/CompactReport.java
@@ -3,12 +3,20 @@ package org.evosuite.utils;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Set;
+
+import org.evosuite.result.BranchInfo;
+import org.evosuite.result.MutationInfo;
 import org.evosuite.result.TestGenerationResultImpl;
 
 public class CompactReport implements Serializable {
     public String UUT;
     public String testSuiteCode;
     public Set<Integer> allCoveredLines;
+    public Set<Integer> allUncoveredLines;
+    public Set<BranchInfo> allCoveredBranches;
+    public Set<BranchInfo> allUncoveredBranches;
+    public Set<MutationInfo> allCoveredMutation;
+    public Set<MutationInfo> allUncoveredMutation;
 
     public HashMap<String, CompactTestCase> testCaseList;
 
@@ -17,6 +25,11 @@ public class CompactReport implements Serializable {
         this.UUT = testGenerationResult.getClassUnderTest();
         this.testSuiteCode = testGenerationResult.getTestSuiteCode();
         this.testCaseList = CompactTestCase.buildTestCaseList(testGenerationResult);
+        this.allUncoveredLines = testGenerationResult.getUncoveredLines();
+        this.allCoveredBranches = testGenerationResult.getCoveredBranches();
+        this.allUncoveredBranches = testGenerationResult.getUncoveredBranches();
+        this.allCoveredMutation = testGenerationResult.getCoveredMutants();
+        this.allUncoveredMutation = testGenerationResult.getUncoveredMutants();
     }
 }
 
